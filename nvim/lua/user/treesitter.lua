8a514c5ad9049bb9f14825b6e0c6f04590d51861
Hannes Neuschmidt <hannes.neuschmidt@brockmann-consult.de>
2025-02-13 09:19:01 +0100

    add config for treesitter context

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -15,3 +15,24 @@ configs.setup({
 	},
 	indent = { enable = true, disable = { "python", "css" } },
 })
+
+local context_ok, context = pcall(require, "treesitter-context")
+if not context_ok then
+    print("treesitter-context not found")
+    return
+end
+
+context.setup({
+    enable = false, -- enable the plugin, can be changed later
+    max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
+    min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+    line_numbers = true,
+    multiline_threshold = 20, -- Maximum number of lines to show for a single context
+    trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+    -- Separator between context and content. Should be a single character string, like '-'.
+    -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+    separator = nil,
+    zindex = 20, -- The Z-index of the context window
+    on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+})
