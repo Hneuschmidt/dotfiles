882c7d992290191b7da3630c89910d180d99cc76
Hannes Neuschmidt <hannes.neuschmidt@brockmann-consult.de>
2025-02-13 09:11:52 +0100

    added custom highlights to colorscheme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -28,9 +28,37 @@ else
     catppuccin.setup({
         styles = {
             conditionals = {},
-        }
+        },
+        custom_highlights = function(colors)
+            return {
+                --TabLineSel = {bg = colors.pink},
+                String = { fg = colors.yellow},
+                Type = { fg = colors.flamingo},
+                Number = { fg = colors.green},
+--                Keyword = { fg = colors.green},
+--                Function = { fg = colors.blue },
+                Operator = {fg = colors.peach},
+--                Conditional = {fg = colors.red},
+--                Return = {bg = colors.red},
+                CursorLine = {bg = colors.surface1},
+                StatusLine = {bg = colors.surface2},
+                StatusLineNC = {bg = colors.surface0, fg = colors.subtext1},
+                DebugPC = {fg = colors.green},
+                DapBreakpoint = {fg = colors.peach},
+                --SignColumn = {fg = colors.green},
+                Comment = {fg = colors.overlay1},
+                --Constant = {fg = colors.pink},
+            }
+        end
     })
 
     vim.cmd.colorscheme "catppuccin"
 end
 
+-- TODO move to treesitter config
+vim.api.nvim_set_hl(0, "@keyword.function.python", { link = "Keyword"})
+vim.api.nvim_set_hl(0, "@type.builtin", { link = "Type"})
+--vim.api.nvim_set_hl(0, "@variable.parameter", { link = "Constant"})
+
+vim.fn.sign_define('DapBreakpoint', {text="", texthl="DapBreakpoint"})
+vim.fn.sign_define('DapStopped', {text = "󰜴", texthl="DebugPC"})
